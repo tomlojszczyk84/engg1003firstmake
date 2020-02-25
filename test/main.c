29d3a460e1e0e99dc5447e64cdfa740f11465155
Tomas Lojszczyk <c3355179@uon.edu.au>
2020-02-25 14:21:35 +1100

    Initial commit

---
+++ test/main.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/*
+int main()
+{
+
+
+int k;
+scanf("%d", &k);
+ printf("You entered: %d\n", k);
+return 0;
+ }
+
+
+ int main()
+   {
+ int k;
+ printf("Enter an integer: ");
+ scanf("%d", &k);
+ k = 2*k;
+ printf("That integer doubled is: %d\n", k);
+ return 0;
+ }
+*/
+
+
+
+
+int main() {
+    float x;
+    float y;
+    printf("Enter a number:");
+    scanf("%f", &x);
+   //y=3*5+(3*5);
+  //y = 9*x +32;
+  //y=x/(1-x);
+//y=(x*x)+2*x;
+ y=(x+2)/(x-1);
+
+
+
+
+
+
+    printf("y: %f\n", y);
+    return 0;
+}
+
+
+
+
+
